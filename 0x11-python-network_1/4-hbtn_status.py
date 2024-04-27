7496e39e0f67e307b2d062e7c4f6287e149b51a5
Omar Ahmed <73648173+Omar2454@users.noreply.github.com>
2024-04-27 18:45:56 +0300

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,8 +4,7 @@ import requests
 
 if __name__ == "__main__":
     # Fetch the URL
-    response = requests.get("https://alx-intranet.hbtn.io/status")
-    
+    response = requests.get("https://alx-intranet.hbtn.io/status") 
     # Print the body of the response
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
