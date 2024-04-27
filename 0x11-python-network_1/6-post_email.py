ad3c02d69c66b64922b62e7963fed2084652251c
Omar Ahmed <73648173+Omar2454@users.noreply.github.com>
2024-04-27 18:28:00 +0300

    asd

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,14 +2,26 @@
 """A script that:
 - takes in a URL,
 - sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
+- of the X-Request-Id variable found in the header of the response.
 """
 import sys
 import urllib.request
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: {} <URL>".format(sys.argv[0]))
+        sys.exit(1)
+
     url = sys.argv[1]
 
+    # Create a request object
     request = urllib.request.Request(url)
+    
+    # Send the request and handle the response
     with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).post("X-Request-Id"))
+        # Get 'X-Request-Id' from the response headers
+        x_request_id = response.headers.get('X-Request-Id')
+        if x_request_id:
+            print(x_request_id)
+        else:
+            print("X-Request-Id not found in response headers.")
